7ad4aeaba1fa97914fcaece90dac6d8692c2c655
Marsel Ishmukhmetov <yamarvin01@yandex.ru>
2025-04-30 18:47:17 +0300

    changes

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { TuiIcon, TuiLink } from '@taiga-ui/core';
+
+@Component({
+  selector: 'app-about',
+  imports: [
+    TuiLink,
+    TuiIcon,
+  ],
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutComponent {
+  constructor(
+    private cd: ChangeDetectorRef,
+  ) {}
+}
