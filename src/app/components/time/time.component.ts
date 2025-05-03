bb8ff7a1f4bcd5ba660d69f65b595843d0d04240
Marsel Ishmukhmetov <yamarvin01@yandex.ru>
2025-05-03 18:12:34 +0300

    Доверстать все что осталось

--- src/app/components/time/time.component.ts
+++ src/app/components/time/time.component.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { TUI_DARK_MODE, TuiIcon } from '@taiga-ui/core';
+import { TUI_DARK_MODE } from '@taiga-ui/core';
 
 @Component({
-  selector: 'app-menu',
-  imports: [CommonModule, TuiIcon],
-  templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss',
+  selector: 'app-time',
+  imports: [CommonModule],
+  templateUrl: './time.component.html',
+  styleUrl: './time.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MenuComponent {
+export class TimeComponent {
   protected readonly darkMode = inject(TUI_DARK_MODE);
 }
