fa87dd256f684385d619ab64635bb7b72ec26524
Marsel Ishmukhmetov <yamarvin01@yandex.ru>
2025-12-07 16:11:14 +0300

    Доработки

--- src/app/components/reviews/reviews.component.ts
+++ src/app/components/reviews/reviews.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { TuiButton, TuiIcon } from '@taiga-ui/core';
-import { TuiCarousel, TuiPagination } from '@taiga-ui/kit';
+import { TuiCarousel } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-reviews',
-  imports: [TuiButton, TuiCarousel, TuiIcon, CommonModule, TuiIcon, TuiPagination],
+  imports: [TuiCarousel, CommonModule],
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -56,13 +55,24 @@ export class ReviewsComponent {
     'assets/reviews/2025-05-10 (4).png',
     'assets/reviews/2025-05-10 (5).png',
     'assets/reviews/2025-05-10.png',
+    'assets/reviews/photo_2025-05-30_21-46-16.jpg',
     'assets/reviews/photo_2025-04-17 00.26.40.jpeg',
+    'assets/reviews/photo_2025-05-21_16-44-22 (2).jpg',
     'assets/reviews/photo_2025-04-17 00.26.44.jpeg',
     'assets/reviews/photo_2025-04-17 00.26.47.jpeg',
     'assets/reviews/photo_2025-04-17 00.26.54.jpg',
     'assets/reviews/photo_2025-05-21_16-38-33.jpg',
-    'assets/reviews/photo_2025-05-21_16-44-22 (2).jpg',
     'assets/reviews/photo_2025-05-21_16-44-22.jpg',
-    'assets/reviews/photo_2025-05-30_21-46-16.jpg',
+    'assets/reviews/photo_2025-12-07 (1).jpg',
+    'assets/reviews/photo_2025-12-07 (2).jpg',
+    'assets/reviews/photo_2025-12-07 (3).jpg',
+    'assets/reviews/photo_2025-12-07 (4).jpg',
+    'assets/reviews/photo_2025-12-07 (5).jpg',
+    'assets/reviews/photo_2025-12-07 (6).jpg',
+    'assets/reviews/Снимок экрана 2025-12-07 (1).png',
+    'assets/reviews/Снимок экрана 2025-12-07 (2).png',
+    'assets/reviews/Снимок экрана 2025-12-07 (3).png',
+    'assets/reviews/Снимок экрана 2025-12-07 (4).png',
+    'assets/reviews/Снимок экрана 2025-12-07 (5).png',
   ];
 }
